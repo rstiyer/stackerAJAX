9ecc8645867d1a9a00c22ae0c4438ec16456ec18
rstiyer <rachel.stiyer@gmail.com>
2016-07-31 13:28:54 -0500

    Change stackoverflow links to use https

--- js/app.js
+++ js/app.js
@@ -78,7 +78,7 @@ var getUnanswered = function(tags) {
 	};
 	
 	$.ajax({
-		url: "http://api.stackexchange.com/2.2/questions/unanswered",
+		url: "https://api.stackexchange.com/2.2/questions/unanswered",
 		data: request,
 		dataType: "jsonp",//use jsonp to avoid cross origin issues
 		type: "GET",
@@ -106,7 +106,7 @@ var getInspiration = function(tag) {
 	};
 
 	$.ajax({
-		url: "http://api.stackexchange.com/2.2/tags/"+ tag +"/top-answerers/all_time",
+		url: "https://api.stackexchange.com/2.2/tags/"+ tag +"/top-answerers/all_time",
 		data: request,
 		dataType: "jsonp",
 		type: "GET"
